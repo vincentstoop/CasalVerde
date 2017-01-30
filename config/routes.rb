88b19b6a5d8c5d4727db1550d90d7b2edf5b1608
TheFullResolution <progreacja@gmail.com>
2017-01-30 21:32:10 +0100

    Added Homepage Model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
 Rails.application.routes.draw do
-# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homepages#index'
 
+  resources :homepages
   resources :reviews
-  
   namespace :admin do
     get '/', to: 'panel#index'
     resources :prices, except: [:show]
