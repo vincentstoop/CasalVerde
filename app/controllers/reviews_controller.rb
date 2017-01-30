ae925befda5fa07fbc2615716919187cc01b2477
StefanKollaart <garbera@icloud.com>
2017-01-30 17:35:40 +0100

    Created new form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,10 +4,22 @@ class ReviewsController < ApplicationController
   end
 
   def new
-
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
 
+    if @review.save
+      redirect_to reviews_path
+    else
+      redirect_to reviews_path
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:name, :stars, :review)
   end
 end
