df8a6fb184d06b2f447c676eb2e83127916915d9
StefanKollaart <garbera@icloud.com>
2017-01-31 15:49:25 +0100

    Fixed create errors

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   validates :name, presence: true, length: {maximum: 50}
   validates :stars, presence: true, inclusion: 1..5, numericality: {only_integer:true}
-  validates :review, presence: true, length: {minimum: 20, maximum: 5000}
+  validates :review, presence: true, length: {minimum: 10, maximum: 5000}
 end
