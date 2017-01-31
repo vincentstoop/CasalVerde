70c77877298f2bf9f5ae12917b6ee0862fbc3672
Thanosky <kthanoss90@gmail.com>
2017-01-31 11:41:41 +0100

    added view and index method to controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,16 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all 
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+  end
+
+private
+  def booking_params
+    params.require(:booking).permit(:check_in, :check_out, :first_name,
+     :last_name, :title, :phone, :email, :street_name, :street_number,
+     :city, :zip_code, :people, :confirmed, :paid, :total_price)
+  end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -14,4 +14,14 @@ class Booking < ApplicationRecord
   validates :confirmed, presence: true
   validates :paid, presence: true
   validates :total_price, presence: true
+
+  def available?(check_in, check_out)
+    bookings.each do |booking|
+      if (booking.starts_at <= check_out) && (booking.ends_at >= check_in)
+        return false
+      end
+    end
+
+    true
+  end
 end
