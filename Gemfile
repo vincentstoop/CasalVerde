6332f8da2d09459f7e2ca7c7d6906ef9f16081ec
Sander Sijbrandij <ssijbrandij@gmail.com>
2017-01-31 09:21:42 +0100

    Add devise

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'rails', '~> 5.0.1'
 gem 'sass-rails', '~> 5.0'
 gem 'turbolinks', '~> 5'
 gem 'uglifier', '>= 1.3.0'
+
 gem 'devise'
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :reviews
-  
+
   namespace :admin do
     get '/', to: 'panel#index'
     resources :prices, except: [:show]
