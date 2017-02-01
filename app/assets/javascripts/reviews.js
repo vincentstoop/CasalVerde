eb8b7c1f785db68184d8e8bc3cd4b6c33f9bc931
StefanKollaart <garbera@icloud.com>
2017-02-01 11:22:56 +0100

    Setting up reviews to use ajax

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,3 +1,19 @@
+// Showing form partial
 $('#show-partial').click(function(){
     $('#partial').show();
 });
+
+// Updating reviews with ajax
+function createReview(name, stars, review) {
+  var liName = $('<ul></ul>')
+
+}
+
+function submitReview(event) {
+  event.preventDefault();
+  createReview($("").val());
+  $("").val(null);
+  console.log("I am submitted");
+}
+
+$("form").bind('commit', submitTodo);
