a3b91f4052ca4139e20cf7b0a997ce4738c69b9b
TheFullResolution <progreacja@gmail.com>
2017-02-02 11:47:36 +0100

    Added Reviews

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,5 +8,6 @@ class PagesController < ApplicationController
     @page = Page.find_by(name: 'home_page')
     @carousel = @page.carousels.find_by(name: 'home')
     @paragraphs = @page.paragraphs.all
+    @reviews = Review.last_3_reviews
   end
 end
