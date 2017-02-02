e65b16bac650272dcd8849b8d1ed0c3f69aa9e1b
TheFullResolution <progreacja@gmail.com>
2017-02-02 15:06:23 +0100

    Mutherfuckin' DatePicker

---
+++ app/assets/javascripts/datepicker.js
@@ -0,0 +1,40 @@
+/* global $ jQuery */
+
+
+function call_takend_dates() {
+    $.ajax({
+            type: 'GET',
+            url: '/bookings/taken'
+        })
+        .done(function(data) {
+            var array = data.dates;
+            $.datepicker.setDefaults({
+                dateFormat: 'yy-mm-dd',
+                minDate: 0,
+                beforeShowDay: function(date) {
+                    var string = jQuery.datepicker.formatDate('yy-mm-dd', date);
+                    return [array.indexOf(string) == -1];
+                }
+            });
+            init_pickers();
+        })
+
+        .fail(function() {
+            $.datepicker.setDefaults({
+                minDate: 0,
+                dateFormat: 'yy-mm-dd'
+            });
+            init_pickers();
+        });
+}
+
+function init_pickers() {
+    $('#checkin').datepicker();
+    $('#checkout').datepicker();
+    $('#booking_check_in').datepicker();
+    $('#booking_check_out').datepicker();
+}
+
+$(document).on('turbolinks:load', function() {
+    call_takend_dates();
+});
