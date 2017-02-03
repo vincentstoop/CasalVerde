4ba2ce38970e8cb4d214a93278e3b1e7994a06e1
StefanKollaart <garbera@icloud.com>
2017-02-03 10:55:08 +0100

    Nested routes pages

--- config/routes.rb
+++ config/routes.rb
@@ -21,8 +21,11 @@ Rails.application.routes.draw do
     resources :prices, except: [:show, :new]
     resources :bookings, only: [:index, :update, :destroy]
     resources :reviews, only: [:index, :destroy]
-    resources :pages
-    resources :paragraphs
-    resources :photos
+    resources :pages do
+      resources :paragraphs
+      resources :carousels do
+        resources :photos
+      end
+    end
   end
 end
