aef40abd66bf5befe1f92f979f604b084b1f8063
StefanKollaart <garbera@icloud.com>
2017-02-03 14:52:32 +0100

    Cleaned up pages admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -25,9 +25,9 @@ Rails.application.routes.draw do
     resources :prices, except: [:show, :new]
     resources :bookings, only: [:index, :update, :delete]
     resources :reviews, only: [:index, :delete]
-    resources :pages do
-      resources :paragraphs
-      resources :photos
+    resources :pages, except: [:show, :edit, :new] do
+      resources :paragraphs, except: [:show, :edit, :new]
+      resources :photos, except: [:show, :edit, :new]
     end
   end
 end
