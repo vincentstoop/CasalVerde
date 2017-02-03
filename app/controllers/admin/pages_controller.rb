f20655d87073e0cefe2b10d25df050386b6dea4f
StefanKollaart <garbera@icloud.com>
2017-02-03 14:02:02 +0100

    Made changing pages nicer

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,6 +1,9 @@
 class Admin::PagesController < Admin::BaseController
   def index
     @pages = Page.all
+    @homepage = Page.find_by(name: "home_page")
+    @carousel = Carousel.find_by(name: "home")
+    @photos = Photo.all
   end
 
   def edit
