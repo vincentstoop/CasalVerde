3c2452c64f7734ba2c922da74f0216635937eb86
StefanKollaart <garbera@icloud.com>
2017-02-03 16:59:19 +0100

    No deleting homepage for you

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170203135726) do
+ActiveRecord::Schema.define(version: 20170203143648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,11 +41,6 @@ ActiveRecord::Schema.define(version: 20170203135726) do
     t.string   "name"
     t.integer  "page_id"
     t.index ["page_id"], name: "index_carousels_on_page_id", using: :btree
-  end
-
-  create_table "homepages", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "pages", force: :cascade do |t|
