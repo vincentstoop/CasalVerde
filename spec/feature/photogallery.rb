bb3106c8f185238314d303e81dc4e6254589d57f
vincentstoop <vincentstoop@gmail.com>
2017-02-03 11:53:46 +0100

    Not working tests

--- spec/feature/photogallery.rb
+++ spec/feature/photogallery.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'View instagram images', js: true do
   scenario 'visit photogallery' do
-    visit pages_photogallery_path
+    visit photogallery_pages_path
     expect(page).to have_css '.ig-image'
   end
 end
