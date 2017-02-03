da0e8c4e55b87cab6db940428f2704ef7c6cf5ec
StefanKollaart <garbera@icloud.com>
2017-02-03 10:55:08 +0100

    Nested routes pages

--- app/controllers/admin/paragraphs_controller.rb
+++ app/controllers/admin/paragraphs_controller.rb
@@ -2,10 +2,13 @@ class Admin::ParagraphsController < Admin::BaseController
 
   def index
     @paragraphs = Paragraph.all.order(id: :asc)
+    @page = Page.find(params[:page_id])
   end
 
   def edit
     @paragraph = Paragraph.find(params[:id])
+    @page = Page.find(params[:page_id])
+    @paragraph.page_id = @page.id
   end
 
   def update
