8925a6a175c9cd7adefe8415a337f705e91d2d53
StefanKollaart <garbera@icloud.com>
2017-02-03 11:14:48 +0100

    Destroy paragraphs

--- app/controllers/admin/paragraphs_controller.rb
+++ app/controllers/admin/paragraphs_controller.rb
@@ -3,12 +3,25 @@ class Admin::ParagraphsController < Admin::BaseController
   def index
     @paragraphs = Paragraph.all.order(id: :asc)
     @page = Page.find(params[:page_id])
+    @paragraph = Paragraph.new
   end
 
   def edit
     @paragraph = Paragraph.find(params[:id])
     @page = Page.find(params[:page_id])
     @paragraph.page_id = @page.id
+  end
+
+  def create
+    @paragraph = Paragraph.new(paragraph_params)
+    @page = Page.find(params[:page_id])
+    @paragraph.page_id = @page.id
+
+    if @paragraph.save
+      redirect_to admin_page_paragraphs_path
+    else
+      render "index"
+    end
   end
 
   def update
@@ -18,6 +31,17 @@ class Admin::ParagraphsController < Admin::BaseController
       redirect_to admin_page_paragraphs_path
     else
       render :edit
+    end
+  end
+
+  def destroy
+    @paragraph = Paragraph.find(params[:id])
+    @page = Page.find(params[:page_id])
+
+    if @paragraph.destroy
+      redirect_to admin_page_paragraphs_path
+    else
+      redirect_to admin_page_paragraphs_path, notice: "Fuck you"
     end
   end
 
